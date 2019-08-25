98a4449582a6212706ebb9f51c3d8b67f8a8005d
Kenneth Yue <kennethyue39@gmail.com>
2019-08-25 14:35:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WhatsApp Electron
+WhatsApp Electron gives a WhatsApp desktop client feel for Linux users by using Electron to wrap WhatsApp Web.
---
+++ index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow, shell } = require('electron')
+
+let window
+const userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.131 Safari/537.36';
+
+function createWindow() {
+  window = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    title: 'WhatsApp',
+    webPreferences: { devTools: false }
+  });
+
+  window.setMenuBarVisibility(false);
+  window.on('closed', () => window = null);
+  window.webContents.on('new-window', (event, url) => {
+    shell.openExternal(url);
+    event.preventDefault();
+  });
+  
+  window.loadURL('https://web.whatsapp.com/', { userAgent });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => app.quit());
